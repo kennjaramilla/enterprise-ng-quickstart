27a8f85e24462e2345bb78f0d013929b6beffef7
John Kenneth Jaramilla <johnkenneth.jaramilla@infor.com>
2021-05-12 21:10:30 +0800

    User observables and snapshot to fetch url parameters ID and Name in designer module.

--- src/app/designer/designer.component.ts
+++ src/app/designer/designer.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-designer',
   templateUrl: './designer.component.html',
   styleUrls: ['./designer.component.css']
 })
-export class DesignerComponent implements OnInit {
+export class DesignerComponent implements OnInit, OnDestroy {
+  list: { id: number; name: string };
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.list = {
+      id: this.route.snapshot.params.id,
+      name: this.route.snapshot.params.name,
+    };
+    console.log(this.list);
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.list.id = params.id;
+          this.list.name = params.name;
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
+
+  navigateBack() {
+    this.router.navigate(['']/*, {relativeTo: this.route}*/);
   }
 
 }
