c7006ea16da8351f434acfb9c839e98d523b1d3e
John Kenneth Jaramilla <johnkenneth.jaramilla@infor.com>
2021-05-18 16:42:02 +0800

    Add other Http methods in the APIGatewayService module.

--- src/app/services/api-gateway.service.ts
+++ src/app/services/api-gateway.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
-/* The {providedIn: 'root'} argument will load this service lazily
+/* 
+ * The {providedIn: 'root'} argument will load this service lazily
  * by Angular (behind the scenes) and redundant code can be removed
  * automatically. This can lead to a better performance and loading speed
 */
@@ -12,21 +13,74 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class ApiGatewayService {
 
-  // Sample API rest service context URL
+  /* Sample API rest service context URL */
   private apiUrl = 'https://reqres.in/api/';
 
   constructor(private http: HttpClient) {}
 
+  /* 
+   * Constructs an observable that, when subscribed, 
+   * causes the configured GET request to execute on the server.
+  */
   get(url: string, options?: {}): Observable<any> {
-    return this.http.get(`${this.apiUrl}${url}`, options)
+    return this.http.get<any>(`${this.apiUrl}${url}`, options)
       .pipe(
         retry(1),
         catchError(this.onError)
       )
   }
 
+  /* 
+   * Constructs an observable that, when subscribed,
+   * causes the configured POST request to execute on the server. 
+   * The server responds with the location of the replaced resource.
+  */
+  post(url: string, body: any, options?: {}): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}${url}`, body, options)
+      .pipe(
+        retry(1),
+        catchError(this.onError)
+      )
+  }
 
+  /* 
+   * Constructs an observable that, when subscribed, 
+   * causes the configured PUT request to execute on the server. 
+   * The PUT method replaces an existing resource with a new set of values.
+  */
+  put(url: string, body: any, options?: {}): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}${url}`, body, options)
+      .pipe(
+        retry(1),
+        catchError(this.onError)
+      )
+  }
 
+  /* 
+   * Constructs an observable that, when subscribed, 
+   * causes the configured PATCH request to execute on the server.
+  */
+  patch(url: string, body: any, options?: {}): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}${url}`, body, options)
+      .pipe(
+        retry(1),
+        catchError(this.onError)
+      )
+  }
+
+  /*
+   * Constructs an observable that, when subscribed, 
+   * causes the configured DELETE request to execute on the server.
+  */
+  delete(url: string, options?: {}): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}${url}`, options)
+      .pipe(
+        retry(1),
+        catchError(this.onError)
+      )
+  }
+
+  /* Simple error handling solution */ 
   onError(error: any) {
     let errorMessage = '';
     if(error.error instanceof ErrorEvent) {
