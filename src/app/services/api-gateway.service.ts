4032e9224400c96155a0d8a0f8916c6cc20b911f
John Kenneth Jaramilla <johnkenneth.jaramilla@infor.com>
2021-05-18 17:03:14 +0800

    Update comments format.

--- src/app/services/api-gateway.service.ts
+++ src/app/services/api-gateway.service.ts
@@ -3,25 +3,27 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
-/* 
+/** 
  * The {providedIn: 'root'} argument will load this service lazily
  * by Angular (behind the scenes) and redundant code can be removed
  * automatically. This can lead to a better performance and loading speed
-*/
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ApiGatewayService {
 
-  /* Sample API rest service context URL */
+  /**
+   *  Sample API rest service context URL
+   */  
   private apiUrl = 'https://reqres.in/api/';
 
   constructor(private http: HttpClient) {}
 
-  /* 
+  /** 
    * Constructs an observable that, when subscribed, 
    * causes the configured GET request to execute on the server.
-  */
+   */
   get(url: string, options?: {}): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}${url}`, options)
       .pipe(
@@ -30,11 +32,11 @@ export class ApiGatewayService {
       )
   }
 
-  /* 
+  /** 
    * Constructs an observable that, when subscribed,
    * causes the configured POST request to execute on the server. 
    * The server responds with the location of the replaced resource.
-  */
+   */
   post(url: string, body: any, options?: {}): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}${url}`, body, options)
       .pipe(
@@ -43,11 +45,11 @@ export class ApiGatewayService {
       )
   }
 
-  /* 
+  /** 
    * Constructs an observable that, when subscribed, 
    * causes the configured PUT request to execute on the server. 
    * The PUT method replaces an existing resource with a new set of values.
-  */
+   */
   put(url: string, body: any, options?: {}): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}${url}`, body, options)
       .pipe(
@@ -56,10 +58,10 @@ export class ApiGatewayService {
       )
   }
 
-  /* 
+  /** 
    * Constructs an observable that, when subscribed, 
    * causes the configured PATCH request to execute on the server.
-  */
+   */
   patch(url: string, body: any, options?: {}): Observable<any> {
     return this.http.patch<any>(`${this.apiUrl}${url}`, body, options)
       .pipe(
@@ -68,10 +70,10 @@ export class ApiGatewayService {
       )
   }
 
-  /*
+  /**
    * Constructs an observable that, when subscribed, 
    * causes the configured DELETE request to execute on the server.
-  */
+   */
   delete(url: string, options?: {}): Observable<any> {
     return this.http.delete<any>(`${this.apiUrl}${url}`, options)
       .pipe(
@@ -80,19 +82,26 @@ export class ApiGatewayService {
       )
   }
 
-  /* Simple error handling solution */ 
+  /**
+   * Simple error handling solution 
+   */ 
   onError(error: any) {
     let errorMessage = '';
     if(error.error instanceof ErrorEvent) {
-      // This will get client-side error
+      /**
+       * This will get client-side error
+       */ 
       errorMessage = error.error.message;
     } else {
-      // This will get server-side error
+      /**
+       * This will get SERVER-side error
+       */ 
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    /* This is a sample implementation only but we can talk
+    /**
+     * This is a sample implementation only but we can talk
      * about creating standards for error messages later on
-    */
+     */
     window.alert(errorMessage);
     return throwError(errorMessage);
  }
